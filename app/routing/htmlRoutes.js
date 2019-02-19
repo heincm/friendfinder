6e5ec80b3aa7b83b5202030e694008c71631e937
heincm <chris.m.hein@gmail.com>
2019-02-18 20:38:37 -0500

    initial commit

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,9 @@
+require('../../server');
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
