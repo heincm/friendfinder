dfc3cb76434eff8f8560b6d127150f15b104cf20
heincm <chris.m.hein@gmail.com>
2019-02-27 22:46:32 -0500

    finished logic. removed console logs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,33 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        let newFriends = req.body;
-        friends.push(newFriends);
-        res.json(newFriends);
+        let bestMatch = {
+            name: "",
+            url: "",
+            scoreDifference: Infinity,
+        }
+
+        let userData = req.body;
+        let userScores = userData.scores
+        let difference = 0;
+
+        for (let i = 0; i < friends.length; i++) {
+            let currentFriend = friends[i]
+
+            for (let j = 0; j < currentFriend.scores.length; j++) {
+                difference += Math.abs(parseInt(currentFriend.scores[j]) - parseInt(userScores[j]));
+            }
+
+            if (difference <= bestMatch.scoreDifference) {
+                bestMatch.name = currentFriend.name;
+                bestMatch.url = currentFriend.url;
+                bestMatch.scoreDifference = difference;
+            }
+            difference = 0;
+        }
+
+        friends.push(userData);
+        res.json(bestMatch);
+
     });
 }
