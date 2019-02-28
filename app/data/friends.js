aa5520df3787397cbed224a45f3d09a617879214
heincm <chris.m.hein@gmail.com>
2019-02-27 22:46:53 -0500

    added placeholder data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,32 @@
-let friends = [{
-    name: 'test data',
-}];
+let friends = [
+    {
+        name: 'Joseph',
+        url: 'http://akc.org/wp-content/uploads/2018/01/staff-pup-training.jpg',
+        scores: ["1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"]
+    },
+    {
+        name: 'Ashleigh',
+        url: 'https://static.scientificamerican.com/sciam/cache/file/D059BC4A-CCF3-4495-849ABBAFAED10456_source.jpg?w=590&h=800&526ED1E1-34FF-4472-B348B8B4769AB2A1',
+        scores: ["5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"]
+    }
+];
 
 module.exports = friends;
