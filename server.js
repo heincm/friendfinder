333c56b19ae80af9888d7b7bc1ac090f1b48a38e
heincm <chris.m.hein@gmail.com>
2019-03-21 16:38:57 -0400

    removed unnecessary lines of code

--- server.js
+++ server.js
@@ -13,13 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/app')))
 
-
-
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
-
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
-
